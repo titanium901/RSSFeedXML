eeeaf8b38150675ccde914ab08f88866f3a836ff
titanium901 <nemes901@hotmail.com>
2019-09-26 13:25:46 +0300

    add FeedParser

--- XMLParseExample/Network/FeedParser.swift
+++ XMLParseExample/Network/FeedParser.swift
@@ -8,6 +8,81 @@
 
 import Foundation
 
-class FeedParser {
+class FeedParser: NSObject, XMLParserDelegate  {
     
+    private var rssItems: [RSSItem] = []
+    private var currentElement = ""
+    private var currentTitle = "" {
+        didSet {
+            currentTitle = currentTitle.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        }
+    }
+    private var currentDescription = "" {
+        didSet {
+            currentDescription = currentDescription.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        }
+    }
+    private var currentPubDate = "" {
+        didSet {
+            currentPubDate = currentPubDate.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        }
+    }
+    private var parserComlitionHandler: (([RSSItem]) -> Void)?
+    
+    func parseFeed(url: String, completionHandler : (([RSSItem]) -> Void)?) {
+        self.parserComlitionHandler = completionHandler
+        
+        let request = URLRequest(url: URL(string: url)!)
+        let urlSession = URLSession.shared
+        let task = urlSession.dataTask(with: request) { (data, response, error) in
+            guard let data = data else {
+                if let error = error {
+                    print(error.localizedDescription)
+                }
+                return
+            }
+            
+            //parse our xml data
+            let parser = XMLParser(data: data)
+            parser.delegate = self
+            parser.parse()
+        }
+        task.resume()
+    }
+    
+    // MARK: - XML Parse Delegate
+    //start parse from element <item>
+    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+        currentElement = elementName
+        if currentElement == "item" {
+            currentTitle = ""
+            currentDescription = ""
+            currentPubDate = ""
+        }
+    }
+    
+    func parser(_ parser: XMLParser, foundCharacters string: String) {
+        switch currentElement {
+        case "title": currentTitle += string
+        case "description": currentDescription += string
+        case "pubDate": currentPubDate += string
+        default : break
+        }
+    }
+    
+    //when end element </item>
+    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        if elementName == "item" {
+            let rssItem = RSSItem(title: currentTitle, description: currentDescription, pubDate: currentPubDate)
+            rssItems.append(rssItem)
+        }
+    }
+    
+    func parserDidEndDocument(_ parser: XMLParser) {
+        parserComlitionHandler?(rssItems)
+    }
+    
+    func parser(_ parser: XMLParser, parseErrorOccurred parseError: Error) {
+        print(parseError.localizedDescription)
+    }
 }
