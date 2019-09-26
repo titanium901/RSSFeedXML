375925db7920379f86c7721b880ce294a3afbd88
titanium901 <nemes901@hotmail.com>
2019-09-26 12:45:09 +0300

    add collectionView

--- XMLParseExample/Controller/ViewController.swift
+++ XMLParseExample/Controller/ViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+}
 
+
+// MARK: UICollectionViewDataSource
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "XMLCell", for: indexPath) as! XMLCell
+        
+        return cell
+    }
+    
+    
+}
+
+// MARK: UICollectionViewDelegate
+extension ViewController: UICollectionViewDelegate {
+    
 }
 
---
+++ XMLParseExample/View/XMLCell.swift
@@ -0,0 +1,15 @@
+//
+//  XMLCell.swift
+//  XMLParseExample
+//
+//  Created by Yury Popov on 26/09/2019.
+//  Copyright © 2019 Iurii Popov. All rights reserved.
+//
+
+import UIKit
+
+class XMLCell: UICollectionViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+}
